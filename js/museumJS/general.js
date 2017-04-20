c37b39258b7a32b48a0f2cfc95293cea6d3d71ca
Bharat Singh <bksingh@g.cofc.edu>
2017-04-20 14:04:25 -0400

    Adding slide up effect

--- js/museumJS/general.js
+++ js/museumJS/general.js
@@ -1,24 +1,59 @@
 $( document ).ready(function() {
   //console.log("Hi");  
 
-var replace;
+	var replace;
 
-$( "input:text" ).focus(function() {
-	replace = $(this).val();
-  	$(this).val("");
-  	//console.log(replace);
+	$( "input:text" ).focus(function() {
+		replace = $(this).val();
+	  	$(this).val("");
+	  	//console.log(replace);
 
-});
+	});
 
-$( "input:text" ).blur(function() {
-	console.log($(this).val());
-	//$(this).val(replace);
-
-
-	if ($(this).val() == "") {
-		$(this).val(replace);
-	}
-});
+	$( "input:text" ).blur(function() {
+		console.log($(this).val());
+		//$(this).val(replace);
 
 
+		if ($(this).val() == "") {
+			$(this).val(replace);
+		}
+	});
+
+    $("#mobile-menu").click(function () {
+
+        if($(this).hasClass("enabled")) {
+
+            $(this).removeClass("enabled");
+
+            $("nav").slideUp();
+
+        }else {
+
+            $(this).addClass("enabled");
+
+            $("nav").slideDown();
+
+        }
+
+    });
+
+    $(window).resize(function () {
+
+        console.log("value " + $(this).width());
+
+        if($(this).width() > 767) {
+
+            $("mobile-menu").removeClass("enabled");
+
+            $("nav").show();
+        }
+        else {
+
+            $("mobile-menu").addClass("enabled");
+
+            $("nav").hide();
+        }
+
+    });
 });
