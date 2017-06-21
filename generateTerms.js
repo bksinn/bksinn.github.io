dfd5540b170aa2b6400176f8a6271f4646e31683
Bharat Singh <bksingh@g.cofc.edu>
2017-06-21 11:34:23 -0400

    Fixing errors

--- generateTerms.js
+++ generateTerms.js
@@ -1,28 +1,22 @@
-//My alterations
-
 var contentManagement = {
     PlaceholderClassName: '.content-management-placeholder',
     pageHasContent: function () {
-        //I'm confused about $(contentManagement.PlaceholderClassName).length >> What is this doing exactly??
         console.log(JSON.stringify($(contentManagement.PlaceholderClassName)) + ": " + $(contentManagement.PlaceholderClassName).length);
         console.log("butthole");
         return $(contentManagement.PlaceholderClassName).length;
     },
     getContent: function (region, contenttype) {
-        //How do you grab specific Terms or Privacy Policy information from correct region based on meta data (region, contenttype)?
-        //Ask Joe ==> Need to create it at least (only GB Terms and Privacy policy info available to me)
-        $.get("https://raw.githubusercontent.com/WittiTech/BharatApp/master/terms.html?token=AUsAap06-XFzywU0A5dpbigYFy_Bhtfyks5ZUcx0wA%3D%3D")
+        $.get("https://raw.githubusercontent.com/WittiTech/BharatApp/master/terms.html?token=AUsAapO_1uw-D_UZjv7x9XnCPulE4GUSks5ZU8wFwA%3D%3D")
             .done (function (data) {
                 console.log(region);
                 console.log(contenttype);
                 //console.log(data);
-
-                $(contentManagement.PlaceholderClassName.html(data);
+                $(contentManagement.PlaceholderClassName).html(data);
             })
-            return region + " " + contenttype;
+        return region + " " + contenttype;
     },
 
-    replaceContent: function (content) {
+    replaceContent: function () {
         $(contentManagement.PlaceholderClassName).html(content);
     },
     processContentReplacement: function () {
@@ -30,9 +24,6 @@ var contentManagement = {
             var region = $(contentManagement.PlaceholderClassName).data('region')
             var contentType = $(contentManagement.PlaceholderClassName).data('contenttype')
 
-            //gets content based on what region the content needs to be in (US, GB, Canada) and what the content type is (either 'Terms' or 'Privacy Policy')
-            //Need to have templates for US, GB, Canada, and the Terms and Privacy Policy
-            //DOM injection will depend on these parameters ^
             var content = contentManagement.getContent(region, contentType);
             console.log("Content " + content);
             console.log("Region " + region);
